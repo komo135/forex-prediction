ee0cc08900a45254e75206597b481a76a9356fb6
komo135 <66017773+komo135@users.noreply.github.com>
2021-07-20 22:26:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setuo(
   description = 'prediction of stock price or forex price',
   author = 'Nagi',
   author_email = 'komoootv@gmail.com',
+  license = "MIT,"
   packages = ['prediction'],
   install_requires = ['tensorflow', 'einops', 'yfinance', 'MetaTrader5', 'numpy', 'pandas', 'sklearn']
 )

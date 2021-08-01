e7fb23612239f4a3a2bbe881c4d9d645cd30186e
komo135 <66017773+komo135@users.noreply.github.com>
2021-08-01 15:43:43 +0900

    Update and rename prediction/data/forex_data.py to prediction/forex_data.py

--- prediction/forex_data.py
+++ prediction/forex_data.py
@@ -3,6 +3,8 @@ import numpy as np
 import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 
+window_size = 120
+
 base = 345600 * 10
 
 m1_args = {"symbol": None, "timeframe": mt5.TIMEFRAME_M1, "start_pos": 0, "count": base}
@@ -14,7 +16,7 @@ h4_args = {"symbol": None, "timeframe": mt5.TIMEFRAME_H4, "start_pos": 0, "count
 d1_args = {"symbol": None, "timeframe": mt5.TIMEFRAME_D1, "start_pos": 0, "count": base // 1440}
 
 
-def gen_data(symbol, window_size, time_args=m1_args):
+def gen_data(symbol, time_args=m1_args, pred_length=1):
     """
     symbol = "EURUSD", "USDJPY"...
     window_size = 30, 60, 120, ...
@@ -37,11 +39,11 @@ def gen_data(symbol, window_size, time_args=m1_args):
     time_x = []
     time_y = []
 
-    for i in range(len(y)):
+    for i in range(len(y) - pred_length + 1):
         if i > window_size:
-            time_x.append(x[i - window_size : i])
-            time_y.append(y[i])
+            time_x.append(x[i - window_size:i])
+            time_y.append(y[i:i+pred_length])
 
-    x, y = np.array(time_x).reshape((-1, window_size, 1)), np.array(time_y).reshape((-1,))
+    x, y = np.array(time_x).reshape((-1, window_size, 1)), np.array(time_y).reshape((-1, pred_length))
     
     return train_test_split(x, y, test_size=0.33, random_state=42)

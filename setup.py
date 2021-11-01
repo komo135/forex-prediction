635efe5cf626942b02dc5809f6e5b7024a4e9e49
komoo <l>
2021-11-01 21:47:24 +0900

    Default Changelist
    Signed-off-by: komoo <l>

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 
-setuo(
-  name = "tensorflow_prediction",
-  version = 1.0,
-  description = 'prediction of stock price or forex price',
-  author = 'Nagi',
-  author_email = 'komoootv@gmail.com',
-  license = "MIT,"
-  packages = ['tensorflow_prediction'],
-  install_requires = ['tensorflow', 'einops', 'yfinance', 'MetaTrader5', 'numpy', 'pandas', 'sklearn']
+setup(
+    name="tensorflow_prediction",
+    version=1.0,
+    description='prediction of stock price or forex price',
+    author='Nagi',
+    author_email='komoootv@gmail.com',
+    license="MIT",
+    packages=['tensorflow_prediction'],
+    install_requires=['tensorflow', 'einops', 'yfinance', 'MetaTrader5', 'numpy', 'pandas', 'sklearn']
 )
